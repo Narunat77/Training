bdae95b5e7360507458ee9637f27232bb7b0a486
Narunat77 <Narunat77@gmail.com>
2024-07-02 05:38:43 +0700

    Create SQLActivityLog.sql

---
+++ SQLActivityLog.sql
@@ -0,0 +1,65 @@
+﻿CREATE PROCEDURE SCBS.Usp_GetData_Activity_Log
+AS
+BEGIN
+
+	IF OBJECT_ID('tempdb..#LoginLogout')IS NOT NULL
+	DROP TABLE #LoginLogout
+
+	IF OBJECT_ID('tempdb..#Acitivity')IS NOT NULL
+	DROP TABLE #Acitivity
+
+	IF OBJECT_ID('tempdb..#Result')IS NOT NULL
+	DROP TABLE #Acitivity
+
+    -- เก็บข้อมูลจาก sp1 ใน temporary table
+    CREATE TABLE #LoginLogout (
+        [Date_Time] datetime2(0),
+        [Log_Type] NVARCHAR(100),
+        [Application_Name] NVARCHAR(100),
+        [Event_Type] NVARCHAR(100),
+        [Source_Address] NVARCHAR(15),
+		[Source_User_IdName] NVARCHAR(64),
+		[Source_Object] NVARCHAR(MAX),
+		[Des_Address] NVARCHAR(15),
+		[Des_User_IdName] NVARCHAR(64),
+		[Des_Object] NVARCHAR(MAX),
+		[Result_Status] NVARCHAR(MAX),
+		[Message] NVARCHAR(MAX)
+    )
+
+    INSERT INTO #LoginLogout
+    EXEC [GlobalBO].[setup].[Usp_FetchUserLoginAuditReport]'2020-09-01','2020-09-01',1, 'COMPVW_USERLOGAUDITRPT_1' , 'ALL', 'ALL', null
+
+    -- เก็บข้อมูลจาก sp2 ใน temporary table
+    CREATE TABLE #Acitivity (
+        [Date_Time] datetime2(0),
+        [Log_Type] NVARCHAR(100),
+        [Application_Name] NVARCHAR(100),
+        [Event_Type] NVARCHAR(100),
+        [Source_Address] NVARCHAR(15),
+		[Source_User_IdName] NVARCHAR(64),
+		[Source_Object] NVARCHAR(MAX),
+		[Des_Address] NVARCHAR(15),
+		[Des_User_IdName] NVARCHAR(64),
+		[Des_Object] NVARCHAR(MAX),
+		[Result_Status] NVARCHAR(MAX),
+		[Message] NVARCHAR(MAX)
+    )
+
+    INSERT INTO #LoginLogout
+    EXEC [GlobalBO].[setup].[Usp_FetchAuditReport] '2020-09-01','2020-09-01',1, 'COMPVW_AUDITRPT_1'
+
+    -- รวมข้อมูลจากทั้งสอง temporary table โดยใช้ UNION ALL
+
+	SELECT 
+	B.*
+	INTO #Result
+	FROM
+	(
+    SELECT *
+    FROM #LoginLogout
+    UNION ALL
+    SELECT *
+    FROM #Acitivity
+	) B
+END
